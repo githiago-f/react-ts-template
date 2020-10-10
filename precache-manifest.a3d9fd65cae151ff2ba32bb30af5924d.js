823f177eb1cde79e301ad003c011167729ee9e08
githiago-f <52945005+githiago-f@users.noreply.github.com>
2020-10-10 18:36:31 +0000

    Deploying to gh-pages from  @ c6823fa6c932f3de123c7f96328b3c5333f77a06 🚀

---
+++ precache-manifest.a3d9fd65cae151ff2ba32bb30af5924d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d49350206867c3c2b4cb7bcb09e662",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0632183234b3cd52611",
+    "url": "/static/js/2.74d29263.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.74d29263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36fc2d49f1b985b1acb0",
+    "url": "/static/js/main.0c453c9a.chunk.js"
+  },
+  {
+    "revision": "7e83f83ddebbc08277f9",
+    "url": "/static/js/runtime-main.37b13f4c.js"
+  }
+]);
